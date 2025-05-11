64db673ad6a30cc12eebf5753d5d9961aa0f2af4
Qinwusui <2064508450@qq.com>
2025-05-11 21:41:33 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Xiaoxiaomeng-server/asr"
 	"Xiaoxiaomeng-server/location"
 	"Xiaoxiaomeng-server/openai"
 	"Xiaoxiaomeng-server/weather"
@@ -12,7 +11,6 @@ import (
 	"net/http"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
@@ -218,67 +216,108 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println(device, "已连接")
-	asrClient, err := asr.Start()
+	// asrClient, err := asr.Start()
 	if err != nil {
 		panic(err)
 	}
 	conn := &Connection{
-		conn:    ws,
-		Name:    device,
-		lock:    sync.Mutex{},
-		asrConn: asrClient,
+		conn: ws,
+		Name: device,
+		lock: sync.Mutex{},
+		// asrConn: asrClient,
 	}
+	connections.lock.Lock()
+	connections.Devices[conn] = true
+	connections.lock.Unlock()
 	go conn.Receive(receiveChan)
-	go func(c *Connection) {
-		for {
-			msgType, msg, err := c.asrConn.ReadMessage()
-			if err != nil {
-				log.Println("从百度读取消息失败", err)
-				time.Sleep(100 * time.Millisecond)
+	// go func(c *Connection) {
+	// 	for {
+	// 		msgType, msg, err := c.asrConn.ReadMessage()
+	// 		if err != nil {
+	// 			log.Println("从百度读取消息失败", err)
+	// 			time.Sleep(100 * time.Millisecond)
 
-				c.conn.Close()
-				c.asrConn.Close()
-				break
-			}
-			if msgType == websocket.TextMessage {
-				log.Println("百度语音转文本返回数据", string(msg))
-				m	 := map[string]any{}
-				err := json.Unmarshal(msg, &m)
-				if err != nil {
-					continue
-				}
+	// 			c.conn.Close()
+	// 			// c.asrConn.Close()
+	// 			break
+	// 		}
+	// 		if msgType == websocket.TextMessage {
+	// 			log.Println("百度语音转文本返回数据", string(msg))
+	// 			m	 := map[string]any{}
+	// 			err := json.Unmarshal(msg, &m)
+	// 			if err != nil {
+	// 				continue
+	// 			}
 
-				if t, ok := m["type"].(string); ok && t == "FIN_TEXT" {
-					if result, ok := m["result"].(string); ok && result != "" {
-						log.Println("百度识别到的最终文本", result)
-						log.Println("发送到Deepseek", result)
-						go func(r string) {
-							data, err := openai.Chat(r, "", "")
-							if err != nil {
-								log.Println("发送到deepseek失败了", err)
-							}
-							if d, ok := data.(map[string]any); ok {
-								if content, ok := d["content"].(string); ok {
-									log.Println("deepseek返回的数据", content)
-									c.conn.WriteMessage(websocket.TextMessage, []byte(content))
-								}
-							}
-						}(result)
-					}
-				}
+	// 			if t, ok := m["type"].(string); ok && t == "FIN_TEXT" {
+	// 				if result, ok := m["result"].(string); ok && result != "" {
+	// 					log.Println("百度识别到的最终文本", result)
+	// 					log.Println("发送到Deepseek", result)
+	// 					go func(r string) {
+	// 						data, err := openai.Chat(r, "", "")
+	// 						if err != nil {
+	// 							log.Println("发送到deepseek失败了", err)
+	// 						}
+	// 						if d, ok := data.(map[string]any); ok {
+	// 							if content, ok := d["content"].(string); ok {
+	// 								log.Println("deepseek返回的数据", content)
+	// 								c.conn.WriteMessage(websocket.TextMessage, []byte(content))
+	// 							}
+	// 						}
+	// 					}(result)
+	// 				}
+	// 			}
 
-			}
+	// 		}
+	// 	}
+	// }(conn)
+	for msg := range receiveChan {
+		data := string(msg.Data)
+		log.Println("收到消息", data)
+		var isNoReply bool
+		//如果字符串中包含isNoReply，则不发送给自己
+		if strings.Contains(data, "_isNoReply") {
+			isNoReply = true
 		}
-	}(conn)
+		for i := range connections.Devices {
+			isNeedRemoveConnection := i.SendPing()
+			if isNeedRemoveConnection {
+				i.conn.Close()
+				delete(connections.Devices, i)
 
+				log.Println("连接已关闭", i.Name)
+				continue
+			}
+
+			if msg.From == i.Name && isNoReply {
+				log.Println("不发送给自己")
+				continue
+			}
+			data = strings.ReplaceAll(data, "_isNoReply", "")
+
+			msg.Data = []byte(data)
+			i.Send(msg)
+		}
+	}
+}
+func (c *Connection) SendPing() bool {
+	err := c.conn.WriteMessage(websocket.PingMessage, []byte("ping"))
+	if err != nil {
+		log.Println(err)
+		if c.conn != nil {
+			_ = c.conn.Close()
+			delete(connections.Devices, c)
+		}
+		log.Println("连接已关闭", c.Name)
+		return true
+	}
+	return false
 }
 func (c *Connection) Send(message Message) {
-
+	log.Println("发送消息到", c.Name, string(message.Data))
 	err := c.conn.WriteMessage(websocket.TextMessage, message.Data)
 	if err != nil {
 		log.Println(err)
-		c.lock.Lock()
-		defer c.lock.Unlock()
 		if c.conn != nil {
 			_ = c.conn.Close()
 			delete(connections.Devices, c)
@@ -294,18 +333,18 @@ func (c *Connection) Receive(receiveChan chan<- Message) {
 		if err != nil {
 			log.Println(err)
 			c.conn.Close()
-			c.asrConn.Close()
+			// c.asrConn.Close()
 			break
 		}
 		//二进制流，直接转发到百度语音识别接口
 		if messageType == websocket.BinaryMessage {
-			err := c.asrConn.WriteMessage(messageType, i)
+			// err :x= c.asrConn.WriteMessage(messageType, i)
 
-			if err != nil {
-				log.Println("语音发送到百度失败", err)
+			// if err != nil {
+			// 	log.Println("语音发送到百度失败", err)
 
-				continue
-			}
+			// 	continue
+			// }
 		}
 		//文本类型，解析成json对象后检查json的类型
 		if messageType == websocket.TextMessage {
@@ -313,4 +352,3 @@ func (c *Connection) Receive(receiveChan chan<- Message) {
 		}
 	}
 }
-
